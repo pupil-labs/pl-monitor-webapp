43c26dfc8149821612fdb41908b5f89c774e237d
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2022-02-11 13:30:29 +0700

    add record button icons

---
+++ src/components/img/RecordReady.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const RecordReady = () => {
+  return (
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="16.0011" cy="16.0011" r="12.4444" fill="#F44336" />
+      <circle
+        cx="16"
+        cy="16"
+        r="15.3458"
+        stroke="#F44336"
+        stroke-width="1.30835"
+      />
+    </svg>
+  );
+};
---
+++ src/components/img/RecordStop.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const RecordStop = () => {
+  return (
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="16" cy="16" r="15" stroke="#F44336" stroke-width="2" />
+      <rect
+        x="10.666"
+        y="10.666"
+        width="10.6667"
+        height="10.6667"
+        rx="2"
+        fill="#F44336"
+      />
+    </svg>
+  );
+};
