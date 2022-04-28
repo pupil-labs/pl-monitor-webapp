08545386756f8546882db61dd3942f659fae908b
Daniel Dourvaris <dourvaris@gmail.com>
2022-04-28 16:49:36 +0300

    Remove unnecessary import

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -45,7 +45,7 @@ import { Alert, AlertColor, Snackbar } from "@mui/material";
 import { ElapsedTimer } from "./ElapsedTimer";
 import { EyeCameraIcon } from "./Icon";
 import { WorldCameraIcon } from "./Icon";
-import { isFirefox, isIOS, isIPhone13 } from "react-device-detect";
+import { isFirefox, isIOS } from "react-device-detect";
 const DEFAULT_FORMAT = Format.RTP_H264;
 
 interface PlayerProps {
